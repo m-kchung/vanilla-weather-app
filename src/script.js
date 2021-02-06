1b7e34076e341634f026c64fc6110914b5a82e87
m-kchung <76528519+m-kchung@users.noreply.github.com>
2021-02-06 18:38:14 +0000

    Activated the unit conversion button

--- src/script.js
+++ src/script.js
@@ -36,10 +36,15 @@ function formathours(timestamp) {
   return formattedhour;
 }
 
+// show today's temperature info
 function displayTemp(response) {
   let city = response.data.name;
   let country = response.data.sys.country;
   let icon = response.data.weather[0].icon;
+
+  celsiusTemperature= response.data.main.temp;
+  feelsLikeTemperature = response.data.main.feels_like
+
 
   document.querySelector("#city-heading").innerHTML = `${city}, ${country}`;
   document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
@@ -59,6 +64,7 @@ function displayTemp(response) {
 
 }
 
+// call today's weather API
 function callWeatherApi(city) {
   let cityName = city;
   let apiKey = "5a9b3c5051ae04b7172dded8be3de831";
@@ -69,5 +75,63 @@ function callWeatherApi(city) {
   axios.get(apiUrl).then(displayTemp);
 }
 
+// convert the units to Fahrenheit
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  let feelsLikeElement = document.querySelector("#feels_like")
+  let tempUnit = document.querySelectorAll("#temp-unit");
+  temperatureElement.innerHTML = Math.round((celsiusTemperature * 9/5) + 32);
+  feelsLikeElement.innerHTML = Math.round((feelsLikeTemperature * 9/5) + 32);
+
+  for (let i=0; i<2; i++) {
+  tempUnit[i].innerHTML = "ºF"
+}
+  // add the active class to the Fahrenheit link
+  fahrenheit.classList.remove("btn-outline-secondary");
+  fahrenheit.classList.add("btn-secondary");
+
+  // remove the active class from the celsius link
+  celsius.classList.remove("btn-secondary")
+  celsius.classList.add("btn-outline-secondary")
+
+};
+
+
+// Temperature - Fahrenheit to Celcsus
+function changeToCelsius (event) {
+  event.preventDefault();
+  let temperature = document.querySelector("#temperature");
+  let feelsLikeElement = document.querySelector("#feels_like");
+  let tempUnit = document.querySelectorAll("#temp-unit");
+  temperature.innerHTML = Math.round(celsiusTemperature);
+  feelsLikeElement.innerHTML = Math.round(feelsLikeTemperature);
+
+  for (let i=0; i<2; i++) {
+  tempUnit[i].innerHTML = "ºC"
+}
+
+  // remove the active class to the Fahrenheit link
+  fahrenheit.classList.remove("btn-secondary");
+  fahrenheit.classList.add("btn-outline-secondary");
+
+  // add the active class from the celsius link
+  celsius.classList.remove("btn-outline-secondary")
+  celsius.classList.add("btn-secondary")
+
+}
+
+// Start
+let celsiusTemperature = null;
+let feelsLikeTemperature = null;
+
 // call Weather API at the start
 callWeatherApi("London");
+
+// change to Fahrenheit
+let fahrenheit = document.querySelector("#convert-to-fahrenheit");
+fahrenheit.addEventListener("click", convertToFahrenheit);
+
+  // Change to Celsius
+let celsius = document.querySelector("#convert-to-celsius");
+celsius.addEventListener("click", changeToCelsius)
