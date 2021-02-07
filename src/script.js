54f5e0d8cc090749da95ad370981c8d6b94c2803
m-kchung <76528519+m-kchung@users.noreply.github.com>
2021-02-07 13:33:34 +0000

    Added the forecast for the next 5 days

--- src/script.js
+++ src/script.js
@@ -1,4 +1,19 @@
 // functions
+function formatShortDays(timestamp) {
+  let date = new Date(timestamp);
+  let days = [
+    "Sun",
+    "Mon",
+    "Tue",
+    "Wed",
+    "Thur",
+    "Fri",
+    "Sat"
+  ]
+  let day = days[date.getDay()]
+  return day;
+}
+
   // Current DateTime
 function formatDate(date) {
 
@@ -9,7 +24,7 @@ function formatDate(date) {
     "Wednesday",
     "Thursday",
     "Friday",
-    "Saturday",
+    "Saturday"
   ];
   let day = days[date.getDay()];
 
@@ -43,13 +58,22 @@ function getCurrentPosition() {
 
 // show position
 function showPosition(response) {
-  let latitude = response.coords.latitude
-  let longitude = response.coords.longitude
-  let apiKey = "5a9b3c5051ae04b7172dded8be3de831";
+  latitude = response.coords.latitude
+  longitude = response.coords.longitude
+  
   let units = "metric";
   let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
   let apiUrl = `${apiEndpoint}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(displayTemp);
+}
+
+// forecast by day - call API
+function forecastbyday (lat, lon) {
+  let units ="metric";
+  let exclude = "current,minutely,hourly,alerts"
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/onecall?"
+  let apiUrl =`${apiEndpoint}lat=${lat}&lon=${lon}&exclude=${exclude}&appid=${apiKey}&units=${units}`
+  axios.get(apiUrl).then(forecastDailyTemp);
 }
 
 
@@ -61,7 +85,6 @@ function displayTemp(response) {
 
   celsiusTemperature= response.data.main.temp;
   feelsLikeTemperature = response.data.main.feels_like
-
 
   document.querySelector("#city-heading").innerHTML = `${city}, ${country}`;
   document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
@@ -79,16 +102,19 @@ function displayTemp(response) {
     )
     document.querySelector("#current-time").innerHTML = formatDate(new Date());
 
+    latitude = response.data.coord.lat;
+    longitude = response.data.coord.lon;
+
+    forecastbyday(latitude, longitude)
 }
 
 
-// Get forecast
+// Get 3-hourly forecast
 function displayForecast (response) {
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = null;
-  let forecast = null;
   forecastTemperature= response.data.list
-  
+  let forecast = null
 
   for (let index = 0; index < 5; index++) {
     forecast = response.data.list[index];
@@ -109,13 +135,96 @@ function displayForecast (response) {
       </div>
     `;
   }
+
+}
+
+// get the next 5 days of forecast
+function forecastDailyTemp (response) {
+  forecastDaily = response.data.daily
+  console.log(forecastDaily)
+
+}
+
+// show next 5 days
+function showNext5Days(event) {
+event.preventDefault();
+let forecastElement = document.querySelector("#forecast");
+let forecastDailyTemp = null
+forecastElement.innerHTML = ""
+
+  for (let i= 1; i < 6; i++) {
+    forecastDailyTemp = forecastDaily[i]
+  forecastElement.innerHTML +=`
+      <div class="col col-formatting">
+        <ul>
+          <li>
+            ${formatShortDays(forecastDailyTemp.dt *1000)}
+          </li>
+          <li>
+            <img src = "http://openweathermap.org/img/wn/${forecastDailyTemp.weather[0].icon}@2x.png" class="forecast-icons">
+          </li>
+          <li>
+          <span id="forecast-temp">${Math.round(forecastDailyTemp.temp.day)}</span><span id="temp-unit">ºC</span>
+          </li>
+        </ul>
+      </div>
+     `;
+}
+  
+  
+
+
+
+    // Remove the active class from the Today's button
+  forecastToday.classList.remove("forecast-active");
+  forecastToday.classList.add("forecast-not-active");
+
+  // add the active class to the next five days button
+  forecast5Days.classList.remove("forecast-not-active")
+  forecast5Days.classList.add("forecast-active")
+}
+
+
+// show today's weather again
+function showForecastToday(event) {
+  event.preventDefault();
+  let forecastElement = document.querySelector("#forecast");
+  let forecastTempToday = null;
+
+  forecastElement.innerHTML = ""
+
+  for (let i= 0; i < 5; i++) {
+    forecastTempToday = forecastTemperature[i];
+
+  forecastElement.innerHTML +=`
+      <div class="col col-formatting">
+        <ul>
+          <li>
+            ${formathours(forecastTempToday.dt * 1000)}
+          </li>
+          <li>
+            <img src = "http://openweathermap.org/img/wn/${forecastTempToday.weather[0].icon}@2x.png" class="forecast-icons">
+          </li>
+          <li>
+          <span id="forecast-temp">${Math.round(forecastTempToday.main.temp)}</span><span id="temp-unit">ºC</span>
+          </li>
+        </ul>
+      </div>
+    `;
+}
+  // Add the active class to the Today's button
+  forecastToday.classList.remove("forecast-not-active");
+  forecastToday.classList.add("forecast-active");
+
+  // Remove the active class from the next five days button
+  forecast5Days.classList.remove("forecast-active")
+  forecast5Days.classList.add("forecast-not-active")
 }
 
 
 // call today's weather API
 function callWeatherApi(city) {
   let cityName = city;
-  let apiKey = "5a9b3c5051ae04b7172dded8be3de831";
   let units = "metric";
   let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather"
   let apiUrl = `${apiEndpoint}?q=${cityName}&units=${units}&appid=${apiKey}`;
@@ -201,6 +310,11 @@ function changeToCelsius (event) {
 let celsiusTemperature = null;
 let feelsLikeTemperature = null;
 let forecastTemperature = null;
+let latitude = null;
+let longitude = null;
+let apiKey = "5a9b3c5051ae04b7172dded8be3de831";
+let forecastDaily = null;
+
 
 // call Weather API at the start
 callWeatherApi("London");
@@ -220,3 +334,11 @@ celsius.addEventListener("click", changeToCelsius)
 // location based weather
 let currentlocation = document.querySelector("#location-icon");
 currentlocation.addEventListener("click", getCurrentPosition)
+
+// Show the next 5 days of forecast
+let forecast5Days = document.querySelector("#forecast-5days");
+forecast5Days.addEventListener("click", showNext5Days);
+
+// show today's forecast
+let forecastToday = document.querySelector("#forecast-today");
+forecastToday.addEventListener("click", showForecastToday);
